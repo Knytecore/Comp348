46825558f38c9c75390e99dcaa6c74106297bd46
LentzSn <46855118+LentzSn@users.noreply.github.com>
2022-05-28 19:15:21 -0400

    Update Q4.lisp
    Some minor mistakes corrected

--- Assignment2/Q4.lisp
+++ Assignment2/Q4.lisp
@@ -1,16 +1,16 @@
 (defun flatten-nums-nodup(flist)
-    (if (null flist)); check if the list is null
+    (if (null flist); check if the list is null
     nil
-    (if (listp flist)); check if it's a list. if yes, will recurse on the head of the list
+    (if (listp flist); check if it's a list. if yes, will recurse on the head of the list
     (findcharacter(remove-duplicates(append (flatten-nums-nodup(car flist))(flatten-nums-nodup (cdr flist)))))
-    (cons flist ())); if it's an atom, it will be added to an empty list
+    (cons flist ())))); if it's an atom, it will be added to an empty list
 
 
 (defun findcharacter (newlist);help to find the character
-    (if (null newlist)); check if the list is null... otherwise I get stack overflow message
+    (if (null newlist); check if the list is null... otherwise I get stack overflow message
     nil
-    (if(not(numberp (car newlist)))); check if it's a character
+    (if(not(numberp (car newlist))); check if it's a character
     (findcharacter(cdr newlist)) ; if yes recurse on the tail of the list
-    (cons(car newlist) (findcharacter(cdr newlist))))
+    (cons(car newlist) (findcharacter(cdr newlist))))))
 
-(print(flatten-nums-nodup '(1 2 (3 1) (a 2.5) (2 4.5) ((1 2)))))
+(step(flatten-nums-nodup '(1 2 (3 1) (a 2.5) (2 4.5) ((1 2)))))
